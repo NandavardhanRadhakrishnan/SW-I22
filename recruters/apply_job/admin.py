39996cbb547e87745c23072d23856c35b301c925
NandavardhanRadhakrishnan <nr3648@srmist.edu.in>
2022-03-17 20:46:04 +0530

    Added monthly email report

--- recruters/apply_job/admin.py
+++ recruters/apply_job/admin.py
@@ -3,5 +3,8 @@ from django.contrib import admin
 # Register your models here.
 
 # register job_seeker model
-from .models import Job_seeker
+from .models import Job_seeker, College, Staff
+
 admin.site.register(Job_seeker)
+admin.site.register(College)
+admin.site.register(Staff)
--- recruters/apply_job/update.py
+++ recruters/apply_job/update.py
@@ -1,8 +1,15 @@
 from datetime import datetime
+from dateutil import relativedelta
 import os
 from apscheduler.schedulers.background import BackgroundScheduler
-# import django
-# django.setup()
+
+
+# for sending email
+from email.mime.message import MIMEMessage
+from email.mime.multipart import MIMEMultipart
+import smtplib, ssl
+from email.mime.text import MIMEText
+from django.template.loader import render_to_string
 
 from .models import College, Staff
 
@@ -10,17 +17,37 @@ retiring_age=65
 threshold_months=6
 
 def update_job():
-    print('iegneg')
     for college in College.objects.all():
         retiring_prof=[]
         for staff in Staff.objects.filter(college_name=college):
-            if staff.DOB.year < datetime.now().year - retiring_age-datetime(month=threshold_months):
+            age=relativedelta.relativedelta(datetime.now(),staff.DOB).years
+            if age>=retiring_age-threshold_months:
                 retiring_prof.append(staff)
-        
-        # TODO send email to college
+            
+        notify_college(retiring_prof)
+
+def notify_college(retiring_profs):
+    port = 587 
+    smtp_server = "smtp.gmail.com"
+    college_email=retiring_profs[0].college_name.email
+    sender_email="sihtesting9@gmail.com"
+    password = "SIHtesting"
+    html = render_to_string('apply_job/mail_template.html',{'professors':retiring_profs})
+
+    message=MIMEMultipart("alternative")
+    message.attach(MIMEText(html, "html"))
+    message['subject'] = "Monthly Report of Retiring Professors"
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.starttls(context=context)
+        server.login(sender_email, password)
+        server.sendmail(sender_email, college_email, message.as_string())
+
+    
 
 def start():
     scheduler=BackgroundScheduler()
     scheduler.add_job(func=update_job,trigger='cron',year='*',month='*',day='last')
-    # scheduler.add_job(func=update_job,trigger='interval',seconds=10)
+    # scheduler.add_job(func=update_job,trigger='interval',seconds=5)
     scheduler.start()
